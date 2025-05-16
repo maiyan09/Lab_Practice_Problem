7057f86b83a43dc89cdc6866e1c3d4a2c6c52ed2
maiyan09 <s.rahman.maiyan@gmail.com>
2025-05-16 23:41:32 +0600

    Create 5)-A.c

---
+++ CSE103/Fall-23/5)-A.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+	int N1, N2;
+
+	FILE *file = fopen("allEven.txt", "w");
+
+	if (file == NULL) {
+		printf("Error opening file.\n");
+		return 1;
+	}
+	else
+	{
+		printf("Enter the starting number (N1): ");
+		scanf("%d", &N1);
+		printf("Enter the ending number (N2): ");
+		scanf("%d", &N2);
+		
+		for (int i = N1; i <= N2; i++) {
+			if (i % 2 == 0) {
+				fprintf(file, "%d ", i);
+			}
+		}
+		fclose(file);
+	}
+
+	return 0;
+}
