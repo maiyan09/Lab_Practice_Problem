41e7f86d3915efebe943f5625b3140c2411876f3
maiyan09 <s.rahman.maiyan@gmail.com>
2025-08-15 12:31:35 +0600

    Update MovieTicket.java

--- Lab Task Java/MovieTicket.java
+++ Lab Task Java/MovieTicket.java
@@ -1,8 +1,25 @@
 public class MovieTicket {
+    //instance variable
     String movieName;
     String showTime;
     int availableSeats;
 
+    //default constructor
+    MovieTicket() {
+        movieName = "Leo Messi";
+        showTime = "2 am";
+    }
+    //Constructor with movie name and show time
+    MovieTicket(String movieName, String showTime) {
+        this.movieName = movieName;
+        this.showTime = showTime;
+    }
+    //Constructor with movie name, show time, and available seats
+    MovieTicket(String movieName, String showTime, int availableSeats) {
+        this.movieName = movieName;
+        this.showTime = showTime;
+        this.availableSeats = availableSeats;
+    }
     void bookTickets(int numberOfTickets) {
         if (availableSeats >= numberOfTickets && numberOfTickets > 0) {
             availableSeats -= numberOfTickets;
@@ -12,13 +29,14 @@ public class MovieTicket {
         }
     }
     void cancelTickets(int numberOfTickets) {
+        if (numberOfTickets > 0)
         availableSeats += numberOfTickets;
     }
     int getAvailableSeats() {
         return availableSeats;
     }
     public void display() {
-        System.err.println("Movie Name: "+movieName);
+        System.out.println("Movie Name: "+movieName);
         System.out.println("Show Time: "+showTime);
         System.out.println("Seats Available: "+ availableSeats);
     }
