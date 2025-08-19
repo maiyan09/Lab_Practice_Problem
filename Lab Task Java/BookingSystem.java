0f50a4c73cc317b741619e970c258ae3e5673ac5
maiyan09 <s.rahman.maiyan@gmail.com>
2025-08-19 19:53:45 +0600

    Update BookingSystem.java

--- Lab Task Java/BookingSystem.java
+++ Lab Task Java/BookingSystem.java
@@ -7,9 +7,16 @@ public class BookingSystem {
         System.out.println("Option 1: Use default constructor, then enter details manually.");
         System.out.println("Option 2: Use constructor with movie name and show time.");
         System.out.println("Option 3: Use constructor with all parameters.");
+        int option;
         System.out.print("Enter your option (1-3): ");
-        int option = sc.nextInt();
+        option = sc.nextInt();
         sc.nextLine();
+        //for invalid option
+        while (option < 1 || option > 3) {
+            System.out.print("Enter your option again (1-3): ");
+            option = sc.nextInt();
+            sc.nextLine();
+        }
 
         switch (option) {
             case 1:
@@ -20,8 +27,7 @@ public class BookingSystem {
                 t.showTime = sc.nextLine();
                 System.out.print("Enter available seats: ");
                 t.availableSeats = sc.nextInt();
-                break;
-        
+                break;    
             case 2:
                 System.out.print("Enter a movie name: ");
                 String movieName = sc.nextLine();
@@ -42,7 +48,7 @@ public class BookingSystem {
                 break;
         }
         while (true) {
-            System.out.println("Showing menu");
+            System.out.println("\nShowing menu");
             System.out.println("1. Book tickets.");
             System.out.println("2. Cancel tickets.");
             System.out.println("3. Display ticket details.");
@@ -51,12 +57,12 @@ public class BookingSystem {
             System.out.print("Enter menu: ");
             int menu = sc.nextInt();
             if (menu == 1) {
-                System.out.print("Enter number of tickes to book: ");
+                System.out.print("Enter number of tickets to book: ");
                 int ticket = sc.nextInt();
                 t.bookTickets(ticket);
             }
             else if (menu == 2) {
-                System.out.print("Enter number of tickes to cancel: ");
+                System.out.print("Enter number of tickets to cancel: ");
                 int ticket = sc.nextInt();
                 t.cancelTickets(ticket);
             }
@@ -69,6 +75,9 @@ public class BookingSystem {
             else if (menu == 5) {
                 System.out.println("Exit");
                 break;
+            }
+            else {
+                System.out.println("Invalid menu. Enter menu Between (1-5)");
             }
         }
     }
