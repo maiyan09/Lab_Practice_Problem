0cdb15c82a69d31173ba9003139d6b335616ecdd
maiyan09 <s.rahman.maiyan@gmail.com>
2025-08-31 19:11:41 +0600

    Update and rename Divisible.java to DivisibleCT1.java

--- CSE201_OOP1 (Mid+Final)/CT Question Solve CSE201/DivisibleCT1.java
+++ CSE201_OOP1 (Mid+Final)/CT Question Solve CSE201/DivisibleCT1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Divisible {
+public class DivisibleCT1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter two number: ");
