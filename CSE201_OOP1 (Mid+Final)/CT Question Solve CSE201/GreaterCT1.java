c7ee2df52ca3ac6f65e1d6f6acfecf6cec3d4012
maiyan09 <s.rahman.maiyan@gmail.com>
2025-08-31 19:12:34 +0600

    Update and rename Greater.java to GreaterCT1.java

--- CSE201_OOP1 (Mid+Final)/CT Question Solve CSE201/GreaterCT1.java
+++ CSE201_OOP1 (Mid+Final)/CT Question Solve CSE201/GreaterCT1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Greater {
+public class GreaterCT1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int arr[] = new int[6];
