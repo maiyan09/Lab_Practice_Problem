ca462e8e33318515a4c5bc16ed8a6299472953ea
maiyan09 <s.rahman.maiyan@gmail.com>
2025-08-31 16:43:33 +0600

    Create TempratureQ1.java

---
+++ CSE201_OOP1 (Mid+Final)/CT Question Solve CSE201/TempratureQ1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TempratureQ1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double temp[] = new double[5];
+        System.out.print("Enter 5 temperature (in Celsius): ");
+        for (int i = 0; i < 5; i++) {
+            temp[i] = sc.nextDouble();
+        }
+        for (int i = 0; i < 5; i++) {
+            if (temp[i] < 10) {
+                System.out.println("Clod");
+            }
+            else if (temp[i] >= 10 && temp[i] <= 25) {
+                System.out.println("Moderate");
+            }
+            else {
+                System.out.println("Hot");
+            } 
+        } 
+    }
+}
