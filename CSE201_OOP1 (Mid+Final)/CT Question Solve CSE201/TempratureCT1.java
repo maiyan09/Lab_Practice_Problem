776cc3bc1a314520e9d194e457827eef5e4341a3
maiyan09 <s.rahman.maiyan@gmail.com>
2025-08-31 19:14:27 +0600

    Update and rename TempratureQ1.java to TempratureCT1.java

--- CSE201_OOP1 (Mid+Final)/CT Question Solve CSE201/TempratureCT1.java
+++ CSE201_OOP1 (Mid+Final)/CT Question Solve CSE201/TempratureCT1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TempratureQ1 {
+public class TempratureCT1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double temp[] = new double[5];
