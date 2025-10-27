905fa09a468ebbf72ae3d4e2a7bc95451760b607
maiyan09 <s.rahman.maiyan@gmail.com>
2025-10-27 16:31:35 +0600

    Create ElectricityBill.java

---
+++ CSE201_OOP1 (Mid+Final)/Spring24 Final Ques. Solve/ElectricityBill.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ElectricityBill {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Total number of units consume:  ");
+        int unit = sc.nextInt();
+        int bill = 0;
+
+        if (unit <= 50) {
+            bill = unit * 3;
+        }
+        else if (unit <= 150) {
+            bill = 150 + ((unit-50) * 5);
+
+        }
+        else if (unit <= 300) {
+            bill = 650 + ((unit-150) * 8);
+        }
+        else if (unit > 300) {
+            bill = 1850 + (unit-300) * 10;
+        }
+        System.out.println("Totoal bill: "+ bill + " taka");
+    }
+}
